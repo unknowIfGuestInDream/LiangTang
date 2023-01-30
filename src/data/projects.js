5940061ad22642e516def8324fbbdffcfcf08e1d
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2023-01-30 11:14:01 +0800

    feat: Improve the content of projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,46 +1,37 @@
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'genCode',
+    subtitle: 'genCode',
+    link: 'https://github.com/unknowIfGuestInDream/genCode',
+    image: '/images/projects/genCode.png',
+    date: '2021-06-18',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'It is used to generate java code through stored procedures of databases such as oracle, '
+      +'and to generate crud stored procedures according to database tables, etc.',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    title: 'javafxTool',
+    subtitle: 'javafxTool',
+    link: 'https://github.com/unknowIfGuestInDream/javafxTool',
+    image: '/images/projects/javafxTool.png',
+    date: '2022-10-16',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      'Scaffolding for javafx development based on JDK17, JavaFx17, controlsfx 11 and maven.',
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
-    desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
+    title: 'Document',
+    subtitle: 'Document',
+    link: 'https://www.tlcsdm.com',
+    image: '/images/projects/document.png',
+    date: '2021-01-11',
+    desc: 'Knowledge base built with docsify.',
   },
   {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+    title: 'tlcsdm-common',
+    subtitle: 'tlcsdm-common',
+    image: '/images/projects/tlcsdm_common.png',
+    date: '2022-04-23',
+    desc: 'Common Toolkit for tlcsdm.',
   },
 ];
 
